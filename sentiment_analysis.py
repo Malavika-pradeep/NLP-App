6871e64c06fc70b8f0afe535aac6a77cf0885579
Malavika Pradeep <87356753+Malavika-pradeep@users.noreply.github.com>
2024-02-12 10:25:16 +0000

    sentiment and dependencies

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,3 +5,21 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
+def preprocessor_text(text):
+    #tokenize text
+    tokens= word_tokenize(text.lower())
+
+
+#download nltk corpus
+nltk.download('all')
+
+# Load the amazon review dataset
+df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/amazon.csv')
+
+
+print('AMAZON REVIEW DATASET: ')
+print(df.head())
+
+
+
+
