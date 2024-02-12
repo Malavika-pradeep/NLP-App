33372241d2c04faa6382eaf84616a387c5aecb5d
Malavika Pradeep <87356753+Malavika-pradeep@users.noreply.github.com>
2024-02-12 09:47:44 +0000

    Dataframe and dependencies

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+sns.set_style("whitegrid")
+plt.style.use("fivethirtyeight")
+
+simple_train= ['call you tonight', 'Call me a cab', 'Please call me...PLEASE!']
+
+vect= CountVectorizer()
+vect.fit(simple_train)
+print(vect.get_feature_names_out())
+
+#transform training data into a document-term matrix
+simple_train_dtm= vect.transform(simple_train)
+print(simple_train_dtm)
+#convert sparse matrix to a dense matrix
+print(simple_train_dtm.toarray())
+
+
+df = pd.DataFrame(simple_train_dtm.toarray(), columns= vect.get_feature_names_out())
+print('TRAIN DATAFRAME:')
+print(df)
+
+simple_test= ["please don't call me"]
+simple_test_dm= vect.transform(simple_test)
+simple_test_dm.toarray()
+
+test_df= pd.DataFrame(simple_test_dm.toarray(), column= vect.get_feature_names_out())
+
+print('TEST DATAFRAME:')
+print(test_df)
