ecc5f3d61171d2c1b97bb4267f40bf9a0c93299c
Malavika Pradeep <87356753+Malavika-pradeep@users.noreply.github.com>
2024-02-12 10:49:07 +0000

    Sentiment Analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,9 +5,22 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-def preprocessor_text(text):
+def preprocess_text(text):
     #tokenize text
     tokens= word_tokenize(text.lower())
+     # Remove stop words
+    filtered_tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+    # Lemmatize the to-kens
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+ 
+    # Join the tokens back into a string
+    processed_text= ' '.join(lemmatized_tokens)
+
+    return processed_text
+
+
 
 
 #download nltk corpus
@@ -19,6 +32,15 @@ df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datas
 
 print('AMAZON REVIEW DATASET: ')
 print(df.head())
+
+
+#Preprocess the review text
+df['reviewText']= df['reviewText'].apply(preprocess_text)
+
+print(df)
+
+#initialize nlt
+analyzer= SentimentIntensityAnalyzer()
 
 
 
